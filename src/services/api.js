bca867860e53a9e21334368570b4c2693d448808
rofaidaessa <rofaidaessa6@gmail.com>
2026-06-01 04:58:20 +0300

    jdjd

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.mawtin.net/api',
+    baseURL: 'https://api.mawtin.net',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
